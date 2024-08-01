68b8b19c5433c05065f6cd0aac75b1934d8530cd
Zanete Dijeva <zanete.dijeva@gmail.com>
2024-08-01 13:58:57 +0100

    Formatters and tutorial

--- amazon_news/polls/migrations/0002_question_author.py
+++ amazon_news/polls/migrations/0002_question_author.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polls', '0001_initial'),
+        ("polls", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='question',
-            name='author',
-            field=models.CharField(default='Anonymous', max_length=200),
+            model_name="question",
+            name="author",
+            field=models.CharField(default="Anonymous", max_length=200),
         ),
     ]
